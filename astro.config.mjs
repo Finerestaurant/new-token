70c22b21381a03bac9791da7b9537a388afe7919
Seegong <anthonypark6904@gmail.com>
2026-01-23 10:25:41 +0900

    feat: Restructure docs and update content

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,15 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'new_token',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				{ label: '서론', autogenerate: { directory: '서론' } },
+				{ label: '핵심철학', autogenerate: { directory: '핵심철학' } },
+				{ label: '가설설정', autogenerate: { directory: '가설설정' } },
+				{ label: '실험설계', autogenerate: { directory: '실험설계' } },
+				{ label: '진행과정', autogenerate: { directory: '진행과정' } },
+				{ label: '참고문헌', autogenerate: { directory: '참고문헌' } },
 			],
 		}),
 	],
